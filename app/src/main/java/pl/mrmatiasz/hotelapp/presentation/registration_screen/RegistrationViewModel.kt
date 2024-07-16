953984f97349c9b83055f15d1f1b027ed8bb7b78
MrMatiasz13 <grzeskowmateusz1328@gmail.com>
2024-07-16 11:06:43 +0200

    [Fix] Internet Issue

--- app/src/main/java/pl/mrmatiasz/hotelapp/presentation/registration_screen/RegistrationViewModel.kt
+++ app/src/main/java/pl/mrmatiasz/hotelapp/presentation/registration_screen/RegistrationViewModel.kt
@@ -103,7 +103,10 @@ class RegistrationViewModel @Inject constructor(
                 when(result) {
                     is Resource.Loading -> _registrationState.send(RegistrationState(isLoading = true))
                     is Resource.Success -> _registrationState.send(RegistrationState(isSuccess = "Sign up success"))
-                    is Resource.Error -> _registrationState.send(RegistrationState(isError = result.message))
+                    is Resource.Error -> {
+                        _registrationState.send(RegistrationState(isError = result.message))
+                        Log.d("SUPABASE_AUTH_ERROR", result.message.toString())
+                    }
                 }
             }
         }
