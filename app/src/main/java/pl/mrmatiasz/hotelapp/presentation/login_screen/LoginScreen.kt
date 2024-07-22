2f01dbb8b92a9d22f70b8d14a59af272a1e126fc
MrMatiasz13 <grzeskowmateusz1328@gmail.com>
2024-07-22 10:37:06 +0200

    Login and Registration UI update

--- app/src/main/java/pl/mrmatiasz/hotelapp/presentation/login_screen/LoginScreen.kt
+++ app/src/main/java/pl/mrmatiasz/hotelapp/presentation/login_screen/LoginScreen.kt
@@ -13,6 +13,7 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.MaterialTheme
@@ -28,6 +29,8 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
@@ -61,16 +64,35 @@ fun TitleSection() {
             .fillMaxWidth()
             .wrapContentHeight()
     ) {
-        Row(
+        Column(
             modifier = Modifier
-                .fillMaxWidth()
-                .padding(16.dp)
+                .wrapContentSize()
         ) {
-            Spacer(modifier = Modifier.width(8.dp))
-            Text(
-                text = "Login",
-                style = MaterialTheme.typography.titleLarge
-            )
+            Row(
+                horizontalArrangement = Arrangement.Center,
+                modifier = Modifier
+                    .fillMaxWidth()
+            ) {
+                Text(
+                    text = "HOTEL APP",
+                    fontWeight = FontWeight.Bold,
+                    style = MaterialTheme.typography.headlineLarge
+                )
+            }
+
+            Spacer(modifier = Modifier.height(16.dp))
+
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(16.dp)
+            ) {
+                Spacer(modifier = Modifier.width(8.dp))
+                Text(
+                    text = "Login",
+                    style = MaterialTheme.typography.titleLarge
+                )
+            }
         }
     }
 }
@@ -82,12 +104,11 @@ fun LoginFieldSection(
 ) {
     Box(
         modifier = Modifier
-        .fillMaxWidth()
-        .wrapContentHeight()
+            .fillMaxWidth()
+            .wrapContentHeight()
     ) {
         Column(
             horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.Center,
             modifier = Modifier
                 .fillMaxSize()
                 .padding(8.dp)
@@ -99,6 +120,8 @@ fun LoginFieldSection(
             val loginState = viewModel.loginState.collectAsState(initial = null)
 
             val context = LocalContext.current
+
+            Spacer(modifier = Modifier.height(16.dp))
 
             FormTextField(
                 value = formState.email,
@@ -161,4 +184,11 @@ fun LoginFieldSection(
             }
         }
     }
+}
+
+
+@Preview(showBackground = true)
+@Composable
+private fun LoginScreenPreview() {
+    TitleSection()
 }
--- app/src/main/java/pl/mrmatiasz/hotelapp/presentation/registration_screen/RegistrationScreen.kt
+++ app/src/main/java/pl/mrmatiasz/hotelapp/presentation/registration_screen/RegistrationScreen.kt
@@ -2,6 +2,8 @@ package pl.mrmatiasz.hotelapp.presentation.registration_screen
 
 import android.widget.Toast
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -10,6 +12,8 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.MaterialTheme
@@ -25,6 +29,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
@@ -47,116 +52,152 @@ fun RegistrationScreen(
 
         Spacer(modifier = Modifier.height(16.dp))
 
-        RegistrationFieldSection(viewModel)
+        RegistrationFieldSection(viewModel, navController)
 
-        Column(
-            horizontalAlignment = Alignment.CenterHorizontally,
-            modifier = Modifier
-                .padding(8.dp)
-                .fillMaxWidth()
-        ) {
-            Text(text = "You already have account?")
-            Text(
-                text = "Login here",
-                color = Color.Blue,
-                modifier = Modifier.clickable {
-                    navController.navigate(LoginScreen)
-                }
-            )
-        }
     }
 }
 
 @Composable
 fun TitleSection() {
-    Row(
+    Box(
         modifier = Modifier
             .fillMaxWidth()
-            .padding(16.dp)
+            .wrapContentHeight()
     ) {
-        Spacer(modifier = Modifier.width(8.dp))
-        Text(
-            text = "Sign Up",
-            style = MaterialTheme.typography.titleLarge
-        )
+        Column(
+            modifier = Modifier
+                .wrapContentSize()
+        ) {
+            Row(
+                horizontalArrangement = Arrangement.Center,
+                modifier = Modifier
+                    .fillMaxWidth()
+            ) {
+                Text(
+                    text = "HOTEL APP",
+                    fontWeight = FontWeight.Bold,
+                    style = MaterialTheme.typography.headlineLarge
+                )
+            }
+
+            Spacer(modifier = Modifier.height(16.dp))
+
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(16.dp)
+            ) {
+                Spacer(modifier = Modifier.width(8.dp))
+                Text(
+                    text = "Sign up",
+                    style = MaterialTheme.typography.titleLarge
+                )
+            }
+        }
     }
 }
 
 @Composable
-fun RegistrationFieldSection(viewModel: RegistrationViewModel) {
-    var passwordVisibility by remember { mutableStateOf(false) }
-
-    val formState = viewModel.formState
-
-    val registrationState = viewModel.registrationState.collectAsState(initial = null)
-    val context = LocalContext.current
-
-    Column(
-        horizontalAlignment = Alignment.CenterHorizontally,
+fun RegistrationFieldSection(
+    viewModel: RegistrationViewModel,
+    navController: NavController
+) {
+    
+    Box(
         modifier = Modifier
             .fillMaxWidth()
-            .padding(8.dp)
+            .wrapContentHeight()
     ) {
-        FormTextField(
-            value = formState.username,
-            onValueChange = { viewModel.onEvent(RegistrationEvent.UsernameChanged(it)) },
-            placeHolder = "Username",
-            errorMessage = formState.usernameError.toString(),
-            isError = formState.usernameError != null
-        )
 
-        FormTextField(
-            value = formState.email,
-            onValueChange = { viewModel.onEvent(RegistrationEvent.EmailChanged(it)) },
-            placeHolder = "Email",
-            errorMessage = formState.emailError.toString(),
-            isError = formState.emailError != null
-        )
+        var passwordVisibility by remember { mutableStateOf(false) }
 
-        FormPasswordFiled(
-            value = formState.password,
-            onValueChange = { viewModel.onEvent(RegistrationEvent.PasswordChanged(it)) },
-            placeHolder = "Password",
-            passwordVisibility = passwordVisibility,
-            onIconClick = { passwordVisibility = !passwordVisibility },
-            errorMessage = formState.passwordError.toString(),
-            isError = formState.passwordError != null
-        )
+        val formState = viewModel.formState
 
-        FormPasswordFiled(
-            value = formState.repeatedPassword,
-            onValueChange = { viewModel.onEvent(RegistrationEvent.RepeatedPasswordChanged(it)) },
-            placeHolder = "Repeat Password",
-            passwordVisibility = passwordVisibility,
-            onIconClick = { passwordVisibility = !passwordVisibility },
-            errorMessage = formState.repeatedPasswordError.toString(),
-            isError = formState.repeatedPasswordError != null
-        )
+        val registrationState = viewModel.registrationState.collectAsState(initial = null)
+        val context = LocalContext.current
 
-        Spacer(modifier = Modifier.height(12.dp))
-
-        Button(
-            colors = ButtonDefaults.buttonColors(LightBlue),
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
             modifier = Modifier
                 .fillMaxWidth()
-                .padding(horizontal = 32.dp),
-            onClick = { viewModel.onEvent(RegistrationEvent.Submit) }
+                .padding(8.dp)
         ) {
-            Text(text = "Sign Up")
-        }
+            FormTextField(
+                value = formState.username,
+                onValueChange = { viewModel.onEvent(RegistrationEvent.UsernameChanged(it)) },
+                placeHolder = "Username",
+                errorMessage = formState.usernameError.toString(),
+                isError = formState.usernameError != null
+            )
 
-        LaunchedEffect(key1 = registrationState.value?.isSuccess) {
-            if(registrationState.value?.isSuccess?.isNotEmpty() == true) {
-                val successMessage = registrationState.value?.isSuccess
-                Toast.makeText(context, "$successMessage", Toast.LENGTH_LONG).show()
+            FormTextField(
+                value = formState.email,
+                onValueChange = { viewModel.onEvent(RegistrationEvent.EmailChanged(it)) },
+                placeHolder = "Email",
+                errorMessage = formState.emailError.toString(),
+                isError = formState.emailError != null
+            )
+
+            FormPasswordFiled(
+                value = formState.password,
+                onValueChange = { viewModel.onEvent(RegistrationEvent.PasswordChanged(it)) },
+                placeHolder = "Password",
+                passwordVisibility = passwordVisibility,
+                onIconClick = { passwordVisibility = !passwordVisibility },
+                errorMessage = formState.passwordError.toString(),
+                isError = formState.passwordError != null
+            )
+
+            FormPasswordFiled(
+                value = formState.repeatedPassword,
+                onValueChange = { viewModel.onEvent(RegistrationEvent.RepeatedPasswordChanged(it)) },
+                placeHolder = "Repeat Password",
+                passwordVisibility = passwordVisibility,
+                onIconClick = { passwordVisibility = !passwordVisibility },
+                errorMessage = formState.repeatedPasswordError.toString(),
+                isError = formState.repeatedPasswordError != null
+            )
+
+            Spacer(modifier = Modifier.height(12.dp))
+
+            Button(
+                colors = ButtonDefaults.buttonColors(LightBlue),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = 32.dp),
+                onClick = { viewModel.onEvent(RegistrationEvent.Submit) }
+            ) {
+                Text(text = "Sign Up")
             }
-        }
+
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally,
+                modifier = Modifier
+                    .padding(8.dp)
+                    .fillMaxWidth()
+            ) {
+                Text(text = "You already have account?")
+                Text(
+                    text = "Login here",
+                    color = Color.Blue,
+                    modifier = Modifier.clickable {
+                        navController.navigate(LoginScreen)
+                    }
+                )
+            }
+            LaunchedEffect(key1 = registrationState.value?.isSuccess) {
+                if(registrationState.value?.isSuccess?.isNotEmpty() == true) {
+                    val successMessage = registrationState.value?.isSuccess
+                    Toast.makeText(context, "$successMessage", Toast.LENGTH_LONG).show()
+                }
+            }
 
 
-        LaunchedEffect(key1 = registrationState.value?.isError) {
-            if(registrationState.value?.isError?.isNotEmpty() == true) {
-                val errorMessage = registrationState.value?.isError
-                Toast.makeText(context, "Error: $errorMessage", Toast.LENGTH_LONG).show()
+            LaunchedEffect(key1 = registrationState.value?.isError) {
+                if(registrationState.value?.isError?.isNotEmpty() == true) {
+                    val errorMessage = registrationState.value?.isError
+                    Toast.makeText(context, "Error: $errorMessage", Toast.LENGTH_LONG).show()
+                }
             }
         }
     }
